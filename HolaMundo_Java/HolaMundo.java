adc7a3b9793cc989391611f1a35a52bafb933d83
Karla Santibáñez <96139227+kbsg01@users.noreply.github.com>
2022-11-04 17:59:30 -0300

    Add files via upload

---
+++ HolaMundo_Java/HolaMundo.java
@@ -0,0 +1,14 @@
+/**
+ * HolaMundo
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        String nombre = "Karla";
+        System.out.println("Mi nombre es " + nombre);
+        int edad = 21;
+        System.out.println("Tengo " + edad + " años");
+        String ciudad = "Alhue, CL";
+        System.out.println("Mi ciudad es " + ciudad); 
+    }
+}
+
