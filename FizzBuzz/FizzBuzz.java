7b8d9a48ec9a269b36b44e70bfd71629bb047933
Karla Santibáñez <96139227+kbsg01@users.noreply.github.com>
2022-11-07 19:00:47 -0300

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+
+    public String fizzBuzz (int numero) {
+            if(numero % 3 == 0 && numero % 5 == 0){
+                return "FizzBuzz";
+            }else if(numero % 3 == 0){
+                return "Fizz";
+            }else  if(numero % 5 == 0){
+                return "Buzz";
+            }else{
+                return numero + "";
+            }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String output = fizzBuzz.fizzBuzz(40);
+        System.out.println(output);
+        output = fizzBuzz.fizzBuzz(30);
+        System.out.println(output);
+        output = fizzBuzz.fizzBuzz(20);
+        System.out.println(output);
+        output = fizzBuzz.fizzBuzz(4);
+        System.out.println(output);
+    }
+}
